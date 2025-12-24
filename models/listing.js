7492753d806bd8d225d8c39d8722ae5174fa954e
MOHAMMAD-OWAIS-15310 <1234owaismohd.12@gmail.com>
2025-12-24 15:26:39 +0530

    created listing (crud operations)

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema= new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description: String,
+    image:{
+        type:String,
+        default: "https://i.ytimg.com/vi/HmFCMCQWbFo/maxresdefault.jpg" ,
+        set : (v)=> v==="" ? "https://i.ytimg.com/vi/HmFCMCQWbFo/maxresdefault.jpg" :v,
+    },
+    price: Number,
+    location : String,
+    country : String
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+module.exports= Listing;
