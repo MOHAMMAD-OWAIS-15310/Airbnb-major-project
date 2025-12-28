1675e8e71b870d5e6725a82686e276166ea7de7e
MOHAMMAD-OWAIS-15310 <1234owaismohd.12@gmail.com>
2025-12-28 22:36:51 +0530

    handles review and listing deletion relation

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
+const Review=require("./review.js");
+const review = require("./review.js");
 
 const listingSchema= new Schema({
     title:{
@@ -22,6 +24,12 @@ const listingSchema= new Schema({
         },
     ],
 });
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id: {$in : listingSchema.reviews}});
+    }
+})
 
 const Listing = mongoose.model("Listing",listingSchema);
 module.exports= Listing;
