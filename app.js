ab268e36d946dc77d65f301b4113a5ad184576e3
MOHAMMAD-OWAIS-15310 <1234owaismohd.12@gmail.com>
2026-01-07 23:58:15 +0530

    resolve conflict

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const ExpressError=require("./utils/ExpressError.js");
 // const {listingSchema , reviewSchema}=require("./schema.js");
 // const Review =require("./models/review.js");
 const session=require("express-session");
+const MongoStore = require('connect-mongo');
 const flash=require("connect-flash");
 const passport= require("passport");
 const LocalStrategy=require("passport-local");
@@ -25,7 +26,10 @@ const listingRouter=require("./routes/listing.js");
 const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl=process.env.ATLASDB_URL;
+console.log("NODE_ENV =", process.env.NODE_ENV);
+console.log("DB URL =", dbUrl);
 main()
     .then(()=>{
         console.log("connected to db")
@@ -35,7 +39,7 @@ main()
     });
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine","ejs");
@@ -45,8 +49,17 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store=MongoStore.create({
+    mongoUrl :dbUrl,
+        secret: process.env.SECRET,
+    touchAfter: 24*3600,
+});
+
+
+
 const sessionOptions={
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave :false,
     saveUninitialized: true,
     cookie: {
@@ -59,6 +72,8 @@ const sessionOptions={
 // app.get("/",(req,res)=>{
 //     res.send("api working");
 // })
+
+
 app.use(session(sessionOptions));
 app.use(flash());
 
